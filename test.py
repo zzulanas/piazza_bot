096076d7f5ceb418846da835b163f8e88d1cdc17
Zachary Zulanas <zzulanas@gmail.com>
2020-06-13 12:59:13 -0700

    first commit

---
+++ test.py
@@ -0,0 +1,33 @@
+from piazza_api import Piazza
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import sys
+
+def main():
+    p = Piazza()
+    p.user_login(email="zzulanas@ucsc.edu", password="ZYTron321!@#")
+
+    piazza_url = urlparse(sys.argv[1])
+
+    class_id = piazza_url.path.split('/')[2]
+    post_num = piazza_url.query.split('=')[1]
+    
+    cse103 = p.network(class_id)
+
+    post = cse103.get_post(post_num)
+
+    question = post["history"][0]["content"]
+
+    print("QUESTION")
+    print(post["history"][0])
+
+    
+    question_text = BeautifulSoup(question, features='lxml')
+
+    answers = post["children"]
+
+    print("ANSWERS")
+    print(answers)
+
+if __name__ == "__main__":
+    main()
